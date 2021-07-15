ba42a33e933097c40144a7be3f0c1f2e8289ccfb
c-helmlinger <86612309+c-helmlinger@users.noreply.github.com>
2021-07-15 09:18:09 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='Neural_Network_Package',
+  author='Kevin Zheng',
+  version='1.0.0',
+  description='Downloads Melbourne stock data and gives a prediction on the price of a house.',
+  packages='[Neural_Network_Package]',
+)
+  
